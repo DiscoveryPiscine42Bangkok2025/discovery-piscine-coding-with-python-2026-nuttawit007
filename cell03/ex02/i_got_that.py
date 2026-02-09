c1748d34e9923b7044fcfec90a2a6a9c9211355e
nuttawit007 <nuttawit.ph@gmail.com>
2026-02-10 02:19:07 +0700

    add : cell03-ex02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+start = input("What you gotta say? : ")
+while True:
+    if start == "STOP" or next == "STOP":
+        break
+    else:
+        next = input("I got that! Anything else? : ")  
+
