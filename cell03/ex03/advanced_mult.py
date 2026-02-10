38ff9a66ae1b37f68cd49ea5542671e80ca96506
nuttawit007 <nuttawit.ph@gmail.com>
2026-02-10 09:10:56 +0700

    fix : cell03-ex03

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -8,4 +8,4 @@ if len(sys.argv) > 1:
 num = 0
 while num <= 10:
     print(f"Table de {num}: {num} {num*1} {num*2} {num*3} {num*4} {num*5} {num*6} {num*7} {num*8} {num*9} {num*10}")
-    num += 1. 
+    num += 1
