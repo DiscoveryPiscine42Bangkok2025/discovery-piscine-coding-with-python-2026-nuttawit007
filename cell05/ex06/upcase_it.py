6ec8b1bd05b16f393082087fd476a0d05b8664b9
nuttawit007 <nuttawit.ph@gmail.com>
2026-02-10 14:55:57 +0700

    add : cell05-ex06

---
+++ cell05/ex06/upcase_it.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    ?> ./upcase_it.py | cat -e
+    none$
+    ?> ./upcase_it.py "initiation" | cat -e
+    INITIATION$
+    ?> ./upcase_it.py 'This exercise is quite easy!' | cat -e
+    THIS EXERCISE IS QUITE EASY!$
+    ?>
+'''
+
+import sys
+
+count_arg = len(sys.argv) - 1
+if count_arg != 1:
+    print("none")
+else:
+    print(sys.argv[1].upper())
