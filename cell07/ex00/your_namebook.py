a640ae2b880fb86a2b8a2a6a57ea4e1d530944e6
nuttawit007 <nuttawit.ph@gmail.com>
2026-02-10 21:58:22 +0700

    add : cell07-ex00

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -2,8 +2,8 @@
 
 def array_of_names(persons):
     names = []
-    for first_name in persons.keys():
-        names.append(first_name)
+    for first_name, last_name in persons.items():
+        names.append(first_name + " " + last_name)
     return names
 
 
